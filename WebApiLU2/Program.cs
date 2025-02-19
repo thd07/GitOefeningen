85efa2db2a9aa8f81d69d9be7141e587331ad90d
thd07 <t.dubbeld07@gmail.com>
2025-02-19 15:18:27 +0100

    i pray this stuff works if not god is dead and we have killed him

--- WebApiLU2/Program.cs
+++ WebApiLU2/Program.cs
@@ -12,12 +12,17 @@ var sqlConnectionString = builder.Configuration["SqlConnectionString"];
 if (string.IsNullOrWhiteSpace(sqlConnectionString))
     throw new InvalidProgramException("Configuration variable SqlConnectionString not found");
 
+var sqlConnectionStringg = builder.Configuration.GetValue<string>("SqlConnectionString");
+var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
+
 builder.Services.AddTransient<IObject2dRepository, Object2dRepository>(o => new Object2dRepository(sqlConnectionString));
 
 
 // regel commetaar
 
 var app = builder.Build();
+app.MapGet("/", () => $"The API is up Connection string found: {(sqlConnectionStringFound ? "yes" : "no")}");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
