cad342a080b5c33e62c531559c6d502d386330f0
thd07 <t.dubbeld07@gmail.com>
2025-03-12 15:52:39 +0100

    push

--- WebApiLU2/Repository/Environment2dRepository.cs
+++ WebApiLU2/Repository/Environment2dRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 using WebApiLU2.Models;
 
 namespace WebApiLU2.Repository
@@ -14,10 +15,17 @@ namespace WebApiLU2.Repository
         {
             using (var sqlConnection = new SqlConnection(sqlConnectionString))
             {
-                var OwnerWorlds = (Query: "SELECT * FROM Environment2d WHERE OwnerId = @OwnerId", new { OwnerId = userId });
-                return (OwnerWorlds);
-            }
+                // Open de verbinding naar de database
+                await sqlConnection.OpenAsync();
 
+                // Voer de query uit en haal de resultaten op
+                var OwnerWorlds = await sqlConnection.QueryAsync<Environment2D>(
+                    "SELECT * FROM Environment2d WHERE OwnerId = @OwnerId",
+                    new { OwnerId = userId });
+
+                // Zet de resultaten om naar een lijst en retourneer
+                return OwnerWorlds.AsList();
+            }
         }
 
     }
