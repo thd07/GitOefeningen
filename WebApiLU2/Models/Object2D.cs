cc5e81ee00ccc94160d9cc0d862c1052c62c032b
thd07 <t.dubbeld07@gmail.com>
2025-06-28 23:17:10 +0200

    update

--- WebApiLU2/Models/Object2D.cs
+++ WebApiLU2/Models/Object2D.cs
@@ -5,15 +5,16 @@ namespace WebApiLU2.Models
     
         public class Object2D
         {
-            public Guid IdObject { get; set; } // Primaire sleutel
+            
             public Guid IdEnvironment { get; set; } // Buitenlandse sleutel naar Environment2D
-            public int PrefabId { get; set; } // Object type ID
-            public int PosX { get; set; } // X-coördinaat
-            public int PosY { get; set; } // Y-coördinaat
-            public int ScaleX { get; set; } // Schaal
-            public int ScaleY { get; set; } // Schaal   
-            public int RotationZ { get; set; } // Rotatie in graden
+            public string PrefabId { get; set; } // Object type ID
+            public float PosX { get; set; } // X-coördinaat
+            public float PosY { get; set; } // Y-coördinaat
+            public float ScaleX { get; set; } // Schaal
+            public float ScaleY { get; set; } // Schaal   
+            public float RotationZ { get; set; } // Rotatie in graden
             public int SortingLayer { get; set; } // Renderlaag
+            public Guid IdObject { get; set; } // Primaire sleutel
     }
 
 
