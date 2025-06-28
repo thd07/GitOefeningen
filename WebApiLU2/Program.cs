1e94d5d583a516bc058335588d04e03fc05c6048
thd07 <t.dubbeld07@gmail.com>
2025-06-28 15:43:04 +0200

    a

--- WebApiLU2/Program.cs
+++ WebApiLU2/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+var sqlConnectionString = builder.Configuration.GetConnectionString("SqlConnectionString");
 if (string.IsNullOrWhiteSpace(sqlConnectionString))
     throw new InvalidProgramException("Configuration variable SqlConnectionString not found");
 
